9e9c3d89406bf7fd37a3d4c03bafa2df5398f034
Ricky Mason <Ricky.Mason@BionicInnovations.com>
2015-07-15 15:50:55 -0400

    First Commit

---
+++ Read_Thermocouple_Set_Temp/Read_Thermocouple_Set_Temp/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace Read_Thermocouple_Set_Temp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.desiredTemp = new System.Windows.Forms.TextBox();
+            this.start_button = new System.Windows.Forms.Button();
+            this.stop_button = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(150, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Desired Temperature (C)";
+            this.label1.Click += new System.EventHandler(this.start_button_Click);
+            // 
+            // desiredTemp
+            // 
+            this.desiredTemp.Location = new System.Drawing.Point(156, 43);
+            this.desiredTemp.Name = "desiredTemp";
+            this.desiredTemp.Size = new System.Drawing.Size(100, 20);
+            this.desiredTemp.TabIndex = 1;
+            this.desiredTemp.Text = "Set Temperature(C)";
+            // 
+            // start_button
+            // 
+            this.start_button.Location = new System.Drawing.Point(111, 254);
+            this.start_button.Name = "start_button";
+            this.start_button.Size = new System.Drawing.Size(75, 23);
+            this.start_button.TabIndex = 2;
+            this.start_button.Text = "Start";
+            this.start_button.UseVisualStyleBackColor = true;
+            this.start_button.Click += new System.EventHandler(this.start_button_Click);
+            // 
+            // stop_button
+            // 
+            this.stop_button.Location = new System.Drawing.Point(271, 254);
+            this.stop_button.Name = "stop_button";
+            this.stop_button.Size = new System.Drawing.Size(75, 23);
+            this.stop_button.TabIndex = 4;
+            this.stop_button.Text = "Stop";
+            this.stop_button.UseVisualStyleBackColor = true;
+            this.stop_button.Click += new System.EventHandler(this.stop_button_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Channel 0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(111, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Channel 1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(240, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Channel 3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(334, 90);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Channel 4";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 148);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Channel 5";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(114, 148);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Channel 6";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(240, 148);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(55, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Channel 7";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(337, 148);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(55, 13);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Channel 8";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 107);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(52, 20);
+            this.textBox1.TabIndex = 13;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(111, 107);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(55, 20);
+            this.textBox2.TabIndex = 14;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(243, 106);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(52, 20);
+            this.textBox3.TabIndex = 15;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(337, 107);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(52, 20);
+            this.textBox4.TabIndex = 16;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(22, 164);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(55, 20);
+            this.textBox5.TabIndex = 17;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(111, 164);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(55, 20);
+            this.textBox6.TabIndex = 18;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(243, 164);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(52, 20);
+            this.textBox7.TabIndex = 19;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(337, 164);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(52, 20);
+            this.textBox8.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 289);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.stop_button);
+            this.Controls.Add(this.start_button);
+            this.Controls.Add(this.desiredTemp);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox desiredTemp;
+        private System.Windows.Forms.Button start_button;
+        private System.Windows.Forms.Button stop_button;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+    }
+}
+
---
+++ Read_Thermocouple_Set_Temp/Read_Thermocouple_Set_Temp/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using MccDaq;
+
+namespace Read_Thermocouple_Set_Temp
+{
+    public partial class Form1 : Form
+    {
+        public const int BLOCKSIZE = 10;
+        public const int NUM_CHANNELS = 8;
+        public const string DEVICE = "2408";
+        bool temp_Read = false;
+        public Form1()
+        {
+            InitializeComponent();
+            stop_button.Enabled = false;
+        }
+
+        private void start_button_Click(object sender, EventArgs e)
+        {
+            temp_Read = true;
+            backgroundWorker1.RunWorkerAsync();
+            start_button.Enabled = false;
+            stop_button.Enabled = true;
+            desiredTemp.Enabled = false;
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            MccDaq.ErrorInfo RetVal;
+            int BoardNum = 0;
+
+            //locate the USB-TC
+            BoardNum = GetBoardNum(DEVICE);
+
+            if (BoardNum == -1)
+            {
+                MessageBox.Show("No USB-{0} detected!", DEVICE);
+                return; //exit program
+            }
+            else
+            {
+                MccBoard daq = new MccDaq.MccBoard(BoardNum);
+                float setTemp = (float)Convert.ToDouble(desiredTemp.Text);
+                bool targetTemp = false;
+                int allSamples = 0;
+                float[] tempData = new float[NUM_CHANNELS];
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
+                string[] filePath = new string[NUM_CHANNELS];
+                StringBuilder[] fileString = new StringBuilder[NUM_CHANNELS];
+                for (int j = 0; j < NUM_CHANNELS; j++)
+                {
+                    string fileDateTime = string.Format("_{0:yyyy-MM-dd_hh-mm}",
+        DateTime.Now);
+                    filePath[j] = "Channel" + j + fileDateTime + ".csv";
+                    fileString[j] = new StringBuilder();
+                    fileString[j].AppendLine(string.Format("Channel{0}", j));
+                    fileString[j].AppendLine(string.Format("Time,Elapsed Time,Temperature (C)"));
+
+                }
+                while (temp_Read == true && targetTemp == false)
+                {
+                    for (int j = 0; j < NUM_CHANNELS; j++)
+                    {
+                        RetVal = daq.TIn(j, TempScale.Celsius, out tempData[j], ThermocoupleOptions.Filter);
+                        IsError(RetVal);
+                        // Get the elapsed time as a TimeSpan value.
+                        TimeSpan ts = stopWatch.Elapsed;
+                        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+                        fileString[j].AppendLine(string.Format("{0},{1},{2}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"), elapsedTime, tempData[j].ToString("0.000").PadLeft(10)));
+                        Console.WriteLine("The temperature is on channel {0}" + " is: {1} " + "at elasped time {2} current time {3} ", j, tempData[j].ToString("0.000").PadLeft(10), elapsedTime, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"));
+                    }
+                    for (int j = 0; j < NUM_CHANNELS; j++)
+                    {
+                        if ((float)tempData[j] >= setTemp)
+                        {
+                            allSamples++;
+                        }
+                    }
+                    this.Invoke((MethodInvoker)delegate()
+                    {
+                        this.textBox1.Text = tempData[0].ToString("0.000");
+                        this.textBox2.Text = tempData[1].ToString("0.000");
+                        this.textBox3.Text = tempData[2].ToString("0.000");
+                        this.textBox4.Text = tempData[3].ToString("0.000");
+                        this.textBox5.Text = tempData[4].ToString("0.000");
+                        this.textBox6.Text = tempData[5].ToString("0.000");
+                        this.textBox7.Text = tempData[6].ToString("0.000");
+                        this.textBox8.Text = tempData[7].ToString("0.000");
+                    });
+                    if (allSamples == NUM_CHANNELS)
+                    {
+
+                        targetTemp = true;
+                        this.Invoke((MethodInvoker)delegate()
+                        {
+                            this.start_button.Enabled = true;
+                            this.desiredTemp.Enabled = true;
+                            this.stop_button.Enabled = false;
+                        });
+                    }
+                    else
+                    {
+                        allSamples = 0;
+                    }
+                    //System.Threading.Thread.Sleep(0); //max rate is 2Hz or 500mS per read.
+                }
+                for (int j = 0; j < NUM_CHANNELS; j++)
+                {
+                    File.WriteAllText(filePath[j].ToString(), fileString[j].ToString());
+                }
+            }
+        }
+
+        /************************************************************************/
+        public static int GetBoardNum(string dev)
+        {
+            for (int BoardNum = 0; BoardNum < 99; BoardNum++)
+            {
+                MccDaq.MccBoard daq = new MccDaq.MccBoard(BoardNum);
+                if (daq.BoardName.Contains(dev))
+                {
+                    Console.WriteLine("USB-{0} board number = {1}", dev, BoardNum.ToString());
+                    daq.FlashLED();
+                    return BoardNum;
+                }
+            }
+            return -1;
+        }
+        /************************************************************************/
+        /************************************************************************/
+
+        public static int IsError(ErrorInfo e)
+        {
+            if (e.Value != 0)
+            {
+                MessageBox.Show(e.Message);
+                return 1;
+            }
+            return 0;
+        }
+
+        private void stop_button_Click(object sender, EventArgs e)
+        {
+            temp_Read = false;
+            this.backgroundWorker1.CancelAsync();
+            start_button.Enabled = true;
+            desiredTemp.Enabled = true;
+            stop_button.Enabled = false;
+
+        }
+
+        /************************************************************************/
+    }
+}
